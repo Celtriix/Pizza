6bd34f11f8b56931a7854e7c15bcab5261824a48
Celtrix <celtrixLP@gmail.com>
2022-01-28 19:10:25 +0100

    Vector and comparisons
    Implemented the functions to convert the order to a vector and the functions to get the similarity of two vectors as well as the function to iterate over the list of orders.

--- Order.js
+++ Order.js
@@ -10,39 +10,50 @@ class Order {
     // fungi = false
 
     constructor(name, mozarella, ham, salami, pepperoni, olives, corn, onions) {
-        this.name = name
-        this.mozarella = mozarella
-        this.ham = ham
-        this.salami = salami
-        this.pepperoni = pepperoni
-        this.olives = olives
-        this.corn = corn
-        this.onions = onions
+        this.name = name;
+        this.mozarella = mozarella;
+        this.ham = ham;
+        this.salami = salami;
+        this.pepperoni = pepperoni;
+        this.olives = olives;
+        this.corn = corn;
+        this.onions = onions;
+    }
+
+    getVector = () => {
+        let out = [];
+        out[0] = (this.mozarella);
+        out[1] = (this.ham);
+        out[2] = this.salami;
+        out[3] = this.pepperoni;
+        out[4] = this.olives;
+        out[5] = this.corn;
+        out[6] = this.onions;
+        return out;
     }
 
     getter = (s) => {
         switch (s) {
             case 'name':
-                return this.name
+                return this.name;
             case 'moz':
-                return this.mozarella
+                return this.mozarella;
             case 'ham':
-                return this.ham
+                return this.ham;
             case 'sal':
-                return this.salami
+                return this.salami;
             case 'pep':
-                return this.pepperoni
+                return this.pepperoni;
             case 'oli':
-                return this.olives
+                return this.olives;
             case 'cor':
-                return this.corn
+                return this.corn;
             case 'oni':
-                return this.onions
+                return this.onions;
             default:
                 return;
         }
     }
-
 }
 
 class Pizza {
@@ -51,30 +62,48 @@ class Pizza {
         this.addOrder(order);
         this.addOrder(order2);
     }
-    constructor(order, order2, order3) {
-        this.addOrder(order);
-        this.addOrder(order2);
-        this.addOrder(order3);
+    //constructor(order, order2, order3) {
+    //    this.addOrder(order);
+    //    this.addOrder(order2);
+    //    this.addOrder(order3);
 
-    }
+    //}
     addOrder = (order) => {
-        this.orders.push(order)
+        this.orders.push(order);
     }
+
 }
 
 class PizzaOrganizer {
     orders = []
-    addOrder = () => {
-
+    addOrder = (order) => {
+      this.orders.push(order);
     }
     pizzas = []
     addPizzas = () => { }
 
 
     getSimilarities = () => {
-        let orderList = this.orders
-        
+        let n = this.orders.length;
+        let dist = Array(n).fill(Array(n).fill(0));
+        for(let i = 0; i<n; i++){
+          for(let j = i; j<n; j++){
+            console.log("i: ", i, "j: ", j)
+            dist[i][j] = this.getDistance(this.orders[i], this.orders[j])
+            console.log(dist[i][j])
+          }
+        }
+        return dist;
     }
 
-
+    getDistance = (order1, order2) => {
+      let out = 0;
+      let out1 = order1.getVector();
+      let out2 = order2.getVector();
+      for(let i=0; i<out1.length;i++){
+        if(out1[i]===out2[i])
+          out += 1;
+      }
+      return Math.round(out*100/out1.length)/100;
+    }
 }
